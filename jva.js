fd5a74a6d6322a946d9efff140c01c9c5d58e202
Asim-Silwal <silwalasim70@gmail.com>
2025-11-26 09:55:43 +0545

    Id card Assignment

---
+++ jva.js
@@ -0,0 +1,44 @@
+// let number = prompt("Enter a number:");
+// if (number % 2 === 0) {
+//     console.log(`${number} is even.`);
+// } else {
+//     console.log(`${number} is odd.`);
+// }
+
+
+// day = "Sunday";
+
+
+// let x = '5';
+// if (x === 5) {
+//     console.log("Inside if");
+// }
+
+// if (month == "September" || day == "Sunday" ){
+// console.log ("Class Day");
+// }
+// else {
+// console.log ("The class is off!")
+// }
+
+//let day1 = prompt("Enter a day: ");
+//let day = day1.toLowerCase();
+//if(day == "saturday") {
+//    console.log("Holiday");
+//}
+//else if (day == "sunday" || day == "wednesday") {
+//    console.log("Web Dev Class Day");
+//}
+//else {
+//    console.log("other class");
+//}
+
+//let number = 2
+
+//for (let i = 1; i <= 10; i++) {
+  //  let output = number + " x " + i + " = " + (number * i)
+    //console.log(output)
+//}
+//const title = document.getElementbyid ('1');
+//title.interHTML='This is Title Added by using JS';
+
